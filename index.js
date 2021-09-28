6066fb0a5c7a02432261540cce94a274ad3e0be8
Aleksandr Kurdiukov <ak.moabit@gmail.com>
2021-09-28 16:39:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+function check(char) {
+  switch (char) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3")
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3")
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3")
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3")
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3")
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3")
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3")
+      audio.play();
+      break;
+    default:
+    break;
+  }
+  buttonAnimation(char);
+}
+
+
+
+
+document.addEventListener("keydown", function(event) {
+  let key = event.key;
+  check(key);
+});
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var char = this.innerHTML;
+
+    check(char);
+  });
+}
+
+
+function buttonAnimation(currentKey) {
+  let button = document.querySelector("." + currentKey);
+  button.classList.add("pressed");
+  button.style.color = "white";
+
+    setTimeout(function() {
+    button.classList.remove("pressed");
+    button.style.color = "orange";
+}, 100);
+};
